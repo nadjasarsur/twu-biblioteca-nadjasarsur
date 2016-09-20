8a0bfdab2abb149ab2a0d565d148f078038b3be3
Nadja Sarsur <nsarsur@thoughtworks.com>
2016-09-20 17:40:15 -0300

    Tests for List Book were done

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,38 @@
 package com.twu.biblioteca;
 
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public static String getWelcomeMessage() {
+        return "Welcome";
+    }
+
+    public static String getAllLibraryBooks(List<String> allLibraryBooks) {
+        if(allLibraryBooks == null || allLibraryBooks.isEmpty()) {
+            return "There are no books!";
+        }
+        else {
+            StringBuilder builder = getStringBuilder(allLibraryBooks);
+            return builder.toString();
+        }
+    }
+
+    private static StringBuilder getStringBuilder(List<String> allLibraryBooks) {
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<allLibraryBooks.size(); i++) {
+            if(i<allLibraryBooks.size()-1)
+                builder.append(allLibraryBooks.get(i)+", ");
+            else{
+                builder.append(allLibraryBooks.get(i));
+            }
+        }
+        return builder;
+    }
+
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -2,12 +2,51 @@ package com.twu.biblioteca;
 
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+
     @Test
     public void test() {
         assertEquals(1, 1);
+    }
+
+    @Test
+    public void shouldWelcomeCustomer(){
+        assertEquals("Welcome", BibliotecaApp.getWelcomeMessage());
+    }
+
+    @Test
+    public void shouldShowAllLibraryBooks(){
+        List<String> allLibraryBooks = new ArrayList<>();
+        allLibraryBooks.add("The host");
+        allLibraryBooks.add("O alquimista");
+        assertEquals("The host, O alquimista", BibliotecaApp.getAllLibraryBooks(allLibraryBooks));
+    }
+
+    @Test
+    public void shouldShowDifferentLibraryBooks(){
+        List<String> allLibraryBooks = new ArrayList<>();
+        allLibraryBooks.add("Harry Potter");
+        allLibraryBooks.add("TDD by example");
+        allLibraryBooks.add("");
+        assertEquals("Harry Potter, TDD by example", BibliotecaApp.getAllLibraryBooks(allLibraryBooks));
+    }
+
+    @Test
+    public void shouldAlertCustomerWhenTheBookListIsEmpty(){
+        List<String> allLibraryBooks = new ArrayList<>();
+        assertEquals("There are no books!", BibliotecaApp.getAllLibraryBooks(allLibraryBooks));
+    }
+
+    @Test
+    public void shouldAlertCustomerWhenTheBookListIsNull(){
+        List<String> allLibraryBooks = null;
+        assertEquals("There are no books!", BibliotecaApp.getAllLibraryBooks(allLibraryBooks));
     }
 }
